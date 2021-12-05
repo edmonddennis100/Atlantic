7ad392920dfd71a955d321ddfe1d7e2584b60e15
edmonddennis100 <86522008+edmonddennis100@users.noreply.github.com>
2021-12-05 21:28:32 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,268 @@
+
+// No one MESS with this first code it holds the whole project together
+var c = 0;
+function bless(){
+  if(c==0){
+    document.getElementById('list').style.display="inline"; c=1;
+  }
+  else if(c==1) {
+    document.getElementById('list').style.display="none"; c=0;
+
+  }
+}
+
+var c = 0;
+function lost(){
+  if(c==0){
+    document.getElementById('frame').style.display="inline"; c=1;
+  }
+  else if(c==1) {
+    document.getElementById('frame').style.display="none"; c=0;
+    
+  }
+}
+
+
+//THIS line of codes deals with the Videos we are workin on
+var a = 0;
+function magic(){
+  //THE FIRST video GOES HERE
+  if(a==0){
+    document.getElementById('vid').src="received_276976110548220.mp4"; a=1;
+  }
+  else if(a==1) {
+    //THE SECOND video GOES HERE
+    document.getElementById('vid').src="received_882302055973187.mp4"; a=2;}
+    else if(a==2) {
+      //THE THIRD Video GOES HERE
+      document.getElementById('vid').src="Greed - Jinmi Abduls Ft. Oxlade (Official Visualizer).mp4"; a=3;}
+      else if(a==3) {
+        //THE LAST IMAGE GOES HERE
+        document.getElementById('vid').src="Harmonize_ft_Joeboy_-_My_mind__-_(Official_Audio)(360p).mp4";a=4;}
+}
+//THIS SECOND LINE OF CODE IS DIFFICULT SO PAY ATTENTION
+var a = 0;
+function magic2(){
+  if(a==1){
+    document.getElementById('vid').src="received_242462584433231.mp4"; a=0;
+  }
+  else if(a==2) {
+    document.getElementById('vid').src="received_276976110548220.mp4"; a=1;}
+    else if(a==3) {
+      document.getElementById('vid').src="received_882302055973187.mp4"; a=2;}
+      else if(a==4) {
+        document.getElementById('vid').src="Greed - Jinmi Abduls Ft. Oxlade (Official Visualizer).mp4"; a=3;}
+}
+/*THIS LINE OF CODE DELAS WITH THE SOUTH AFRICA PAGE*/
+var a = 0;
+function southafrica(){
+  //THE FIRST IMAGE GOES HERE
+  if(a==0){
+    document.getElementById('change').src="IMG_20210711_115134_715.jpg"; a=1;
+  }
+  else if(a==1) {
+    //THE SECOND IMAGE GOES HERE
+    document.getElementById('change').src="IMG_20210711_115152_607.jpg"; a=2;}
+    else if(a==2) {
+      //THE THIRD IMAGE GOES HERE
+      document.getElementById('change').src="IMG_20210711_115159_566.jpg"; a=3;}
+      else if(a==3) {
+        //THE LAST IMAGE GOES HERE
+        document.getElementById('change').src="IMG_20210711_115211_365.jpg";a=4;}
+        else if(a==4) {
+          document.getElementById('change'). src="IMG_20210711_115244_544.jpg";a=5;}
+          else if(a==5) {
+            document.getElementById('change'). src="IMG_20210711_115241_434.jpg";a=6;}
+            else if(a==6) {
+              document.getElementById('change'). src="IMG_20210711_115250_449.jpg";a=7;}
+              else if(a==7) {
+                document.getElementById('change'). src="IMG_20210725_124400_782.jpg";a=8;}
+                else if(a==8) {
+                  document.getElementById('change'). src="IMG_20210725_124326_459.jpg";a=9;}
+                  else if(a==9) {
+                    document.getElementById('change'). src="IMG_20210725_124306_931.jpg";a=10;}
+                    else if(a==10) {
+                      document.getElementById('change'). src="IMG_20210725_124246_583.jpg";a=11;}
+                      else if(a==11) {
+                        document.getElementById('change'). src="IMG_20210725_124125_049.jpg";a=12;}
+                        else if(a==12) {
+                          document.getElementById('change'). src="IMG_20210725_124119_548.jpg";a=13;}
+                          else if(a==13) {
+                            document.getElementById('change'). src="IMG_20210725_080321_679.jpg";a=14;}
+                            else if(a==14) {
+                              document.getElementById('change'). src="IMG_20210725_080317_787.jpg";a=15;}
+                              else if(a==15) {
+                                document.getElementById('change'). src="IMG_20210725_080227_845.jpg";a=16;}
+                                else if(a==16) {
+                                  document.getElementById('change'). src="IMG_20210725_080222_299.jpg";a=17;}
+                                  else if(a==17) {
+                                    document.getElementById('change'). src="IMG_20210725_080217_245.jpg";a=18;}
+                                    else if(a==18) {
+                                      document.getElementById('change'). src="IMG_20210725_080154_727.jpg";a=19;}
+                                      else if(a==19) {
+                                        document.getElementById('change'). src="IMG_20210725_080149_066.jpg";a=20;}
+                                        else if(a==20) {
+                                          document.getElementById('change'). src="IMG_20210725_080144_636.jpg";a=21;}
+                                          else if(a==21) {
+                                            document.getElementById('change'). src="IMG_20210712_191258_815.jpg";a=22;}
+                        
+}
+//THIS SECOND LINE OF CODE IS DIFFICULT SO PAY ATTENTION
+var a = 0;
+function southafrica2(){
+  if(a==1){
+    document.getElementById('change').src="IMG_20210717_164304_053.jpg"; a=0;
+  }
+if(a==2){
+    document.getElementById('change').src="IMG_20210711_115134_715.jpg"; a=1;
+  }
+  else if(a==3) {
+    //THE SECOND IMAGE GOES HERE
+    document.getElementById('change').src="IMG_20210711_115152_607.jpg"; a=2;}
+    else if(a==4) {
+      //THE THIRD IMAGE GOES HERE
+      document.getElementById('change').src="IMG_20210711_115159_566.jpg"; a=3;}
+      else if(a==5) {
+        //THE LAST IMAGE GOES HERE
+        document.getElementById('change').src="IMG_20210711_115211_365.jpg";a=4;}
+        else if(a==6) {
+          document.getElementById('change'). src="IMG_20210711_115244_544.jpg";a=5;}
+          else if(a==7) {
+            document.getElementById('change'). src="IMG_20210711_115241_434.jpg";a=6;}
+            else if(a==8) {
+              document.getElementById('change'). src="IMG_20210711_115250_449.jpg";a=7;}
+              else if(a==9) {
+                document.getElementById('change'). src="IMG_20210725_124400_782.jpg";a=8;}
+                else if(a==10) {
+                  document.getElementById('change'). src="IMG_20210725_124326_459.jpg";a=9;}
+                  else if(a==11) {
+                    document.getElementById('change'). src="IMG_20210725_124306_931.jpg";a=10;}
+                    else if(a==12) {
+                      document.getElementById('change'). src="IMG_20210725_124246_583.jpg";a=11;}
+                      else if(a==13) {
+                        document.getElementById('change'). src="IMG_20210725_124125_049.jpg";a=12;}
+                        else if(a==14) {
+                          document.getElementById('change'). src="IMG_20210725_124119_548.jpg";a=13;}
+                          else if(a==15) {
+                            document.getElementById('change'). src="IMG_20210725_080321_679.jpg";a=14;}
+                            else if(a==16) {
+                              document.getElementById('change'). src="IMG_20210725_080317_787.jpg";a=15;}
+                              else if(a==17) {
+                                document.getElementById('change'). src="IMG_20210725_080227_845.jpg";a=16;}
+                                else if(a==18) {
+                                  document.getElementById('change'). src="IMG_20210725_080222_299.jpg";a=17;}
+                                  else if(a==19) {
+                                    document.getElementById('change'). src="IMG_20210725_080217_245.jpg";a=18;}
+                                    else if(a==20) {
+                                      document.getElementById('change'). src="IMG_20210725_080154_727.jpg";a=19;}
+                                      else if(a==21) {
+                                        document.getElementById('change'). src="IMG_20210725_080149_066.jpg";a=20;}
+                                        else if(a==22) {
+                                          document.getElementById('change'). src="IMG_20210725_080144_636.jpg";a=21;}
+                                          else if(a==23) {
+                                            document.getElementById('change'). src="IMG_20210712_191258_815.jpg";a=22;}
+                        
+
+}
+
+/*THIS LINE OF CODE DELAS WITH THE NIGERIA PAGE*/
+var a = 0;
+function nigeria(){
+  //THE FIRST IMAGE GOES HERE
+  if(a==0){
+    document.getElementById('change').src="IMG-20210622-WA0023.jpg"; a=1;
+  }
+  else if(a==1) {
+    //THE SECOND IMAGE GOES HERE
+    document.getElementById('change').src="IMG-20210622-WA0019.jpg"; a=2;}
+    else if(a==2) {
+      //THE THIRD IMAGE GOES HERE
+      document.getElementById('change').src="IMG-20210622-WA0018.jpg"; a=3;}
+      else if(a==3) {
+        //THE LAST IMAGE GOES HERE
+        document.getElementById('change').src="IMG-20210622-WA0025.jpg";a=4;}
+}
+//THIS SECOND LINE OF CODE IS DIFFICULT SO PAY ATTENTION
+var a = 0;
+function nigeria2(){
+  if(a==1){
+    document.getElementById('change').src="IMG-20210622-WA0028.jpg"; a=0;
+  }
+  else if(a==2) {
+    document.getElementById('change').src="IMG-20210622-WA0023.jpg"; a=1;}
+    else if(a==3) {
+      document.getElementById('change').src="IMG-20210622-WA0019.jpg"; a=2;}
+      else if(a==4) {
+        document.getElementById('change').src="IMG-20210622-WA0018.jpg"; a=3;}
+}
+
+
+/*THIS LINE OF CODE DELAS WITH THE Sierra Leone PAGE*/
+var a = 0;
+function sierra(){
+  //THE FIRST IMAGE GOES HERE
+  if(a==0){
+    document.getElementById('change').src="sierra_leone_0.jpg"; a=1;
+  }
+  else if(a==1) {
+    //THE SECOND IMAGE GOES HERE
+    document.getElementById('change').src="n1pansrfj1t01.jpg"; a=2;}
+    else if(a==2) {
+      //THE THIRD IMAGE GOES HERE
+      document.getElementById('change').src="panorama-view-freetown-sierra-leone-260nw-1780663949.jpg"; a=3;}
+      else if(a==3) {
+        //THE LAST IMAGE GOES HERE
+        document.getElementById('change').src="istockphoto-174703540-612x612.jpg";a=4;}
+}
+//THIS SECOND LINE OF CODE IS DIFFICULT SO PAY ATTENTION
+var a = 0;
+function sierra2(){
+  if(a==1){
+    document.getElementById('change').src="freetown.jpg"; a=0;
+  }
+  else if(a==2) {
+    document.getElementById('change').src="Sierra Leone/sierra_leone_0.jpg"; a=1;}
+    else if(a==3) {
+      document.getElementById('change').src="Sierra Leone/n1pansrfj1t01.jpg"; a=2;}
+      else if(a==4) {
+        document.getElementById('change').src="Sierra Leone/panorama-view-freetown-sierra-leone-260nw-1780663949.jpg"; a=3;}
+}
+
+
+/*THIS LINE OF CODE DELAS WITH THE Ghana PAGE*/
+var a = 0;
+function ghana(){
+  //THE FIRST IMAGE GOES HERE
+  if(a==0){
+    document.getElementById('change').src="GGhana/depositphotos_1093316-stock-photo-boats-on-the-beach-on.jpg"; a=1;
+  }
+  else if(a==1) {
+    //THE SECOND IMAGE GOES HERE
+    document.getElementById('change').src="Ghana1.jpg"; a=2;}
+    else if(a==2) {
+      //THE THIRD IMAGE GOES HERE
+      document.getElementById('change').src="girls-dancing-at-an-official-public-ceremony-in-ho-ghana-africa-AG812X.jpg"; a=3;}
+      else if(a==3) {
+        //THE LAST IMAGE GOES HERE
+        document.getElementById('change').src="Ghana2.jpg";a=4;}
+}
+//THIS SECOND LINE OF CODE IS DIFFICULT SO PAY ATTENTION
+var a = 0;
+function ghana2(){
+  if(a==1){
+    document.getElementById('change').src="depositphotos_1093316-stock-photo-boats-on-the-beach-on.jpg"; a=0;
+  }
+  else if(a==2) {
+    document.getElementById('change').src="depositphotos_1093316-stock-photo-boats-on-the-beach-on.jpg"; a=1;}
+    else if(a==3) {
+      document.getElementById('change').src="istockphoto-108203368-612x612.jpg"; a=2;}
+      else if(a==4) {
+        document.getElementById('change').src="girls-dancing-at-an-official-public-ceremony-in-ho-ghana-africa-AG812X.jpg"; a=3;}
+}
+var z=1
+function iframe(){
+  if(z==1) {
+    document.getElementById('inspire'). innerHTML= '<iframe id="vid" src="www.youtube.com/embed/Q4" allowfullscreen frameborder="0"></iframe> <img onclick="magic2()" class="button" src="Button.png"/><img onclick="magic()" class="button1" src="Button.png"/>'; z=0}
+    else if(z==0) {
+      document.getElementById('inspire'). innerHTML= '<p align="center">Inspired by the beauty of art and entertainment<br> <v> At Atlantic Beach we give you the best services your money can buy <br> Contact us </v> <br> <span style="font-size: 90px;" class="playful">TODAY</span></p>'; z=1 }
+    }
